436e484fc94baabf3a9efc3fe9d6d060e1a2787f
Obimbo07 <obimboausts@gmail.com>
2023-05-22 21:00:55 +0300

    linters fix

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const hambergerbtn = document.querySelector('.hamberger-btn');
 const dropdown = document.querySelector('.drop-down');
-hambergerbtn.addEventListener('click',  function() {
+hambergerbtn.addEventListener('click', () => {
   dropdown.style.display = (dropdown.style.display === 'none') ? 'block' : 'none';
 });
