bd858733e1e2963ec679a2aaa3ba2896c31dde42
Obimbo07 <obimboausts@gmail.com>
2023-05-24 02:57:18 +0300

    linter fix

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const popup = document.getElementById('navMenu');
 const navMenu = document.getElementById('navMenu');
-const menudissapear = document.getElementById("nav-btn");
+const menudissapear = document.getElementById('nav-btn');
 
 function openMenu() {
   popup.classList.add('dropdown-menuShow');
   navMenu.classList.add('navMenu');
-  menudissapear.classList.add('navMenu')
+  menudissapear.classList.add('navMenu');
 }
 openMenu();
 function closeMenu() {
   popup.classList.remove('dropdown-menuShow');
   navMenu.classList.remove('navMenu');
-  menudissapear.classList.remove('navMenu')
+  menudissapear.classList.remove('navMenu');
 }
-closeMenu(); 
-
+closeMenu();
