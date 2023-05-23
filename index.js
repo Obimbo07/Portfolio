8ab8b4c3cddc6b3ccd1c41abc826f9942abc4a1d
Kingsley Igbor <chidoziek2@gmail.com>
2023-05-24 00:42:30 +0100

    added close function for the modal links

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const popup = document.getElementById('navMenu');
 const navMenu = document.getElementById('navMenu');
+const menudissapear = document.getElementById("nav-btn");
 
 function openMenu() {
   popup.classList.add('dropdown-menuShow');
   navMenu.classList.add('navMenu');
+  menudissapear.classList.add('navMenu')
 }
 openMenu();
 function closeMenu() {
   popup.classList.remove('dropdown-menuShow');
   navMenu.classList.remove('navMenu');
+  menudissapear.classList.remove('navMenu')
 }
-closeMenu();
+closeMenu(); 
+
+// function menudissapear() {
+//   menudissapear.classList.add('menu-dissapear')
+// }
