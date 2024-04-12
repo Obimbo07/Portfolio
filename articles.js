7533312b5e501de601b2234a318fdb21845204bf
Obimbo07 <obimboausts@gmail.com>
2024-04-13 02:40:37 +0300

    created articles page

---
+++ articles.js
@@ -0,0 +1,29 @@
+const articles = [
+  {
+    id: 1,
+    name: 'John',
+  },
+  {
+    id: 2,
+    name: 'John',
+  },
+  {
+    id: 3,
+    name: 'John',
+  },
+  {
+    id: 4,
+    name: 'John',
+  },
+  {
+    id: 5,
+    name: 'John',
+  },
+  {
+    id: 6,
+    name: 'John',
+  },
+];
+
+const articleSection = document.getElementsByClassName('articles-section');
+const 
