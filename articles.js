f8494fc6d70801cdad14987c6a33ac172c4145b4
Obimbo07 <obimboausts@gmail.com>
2024-04-25 18:57:41 +0300

    added rendered articles

--- articles.js
+++ articles.js
@@ -1,45 +1,41 @@
 const articles = [
   {
     id: 0,
-    name: 'John',
+    name: 'Impact of AI on Programmers: Navigating Standard Shifts',
+    description: 'As a human being, one has been endowed with just enough intelligence to be able to see clearly',
+    image: '/img/technology.jpg',
+    link: 'https://medium.com/@obimboausts/impact-of-ai-on-programmers-navigating-standard-shifts-0e61830570cd'
   },
   {
     id: 1,
-    name: 'John',
-  },
-  {
-    id: 2,
-    name: 'John',
-  },
-  {
-    id: 3,
-    name: 'John',
-  },
-  {
-    id: 4,
-    name: 'John',
-  },
-  {
-    id: 5,
-    name: 'John',
-  },
+    name: 'Guiding the Next Generation: A Mentor’s Thoughts!!',
+    description: 'Mentoring juniors and non-techies into the world of software development is a journey fraught with challenges and rewards.',
+    image: '/img/gas.png',
+    link: 'full_article.html'
+  }
 ];
 
-function articleCards(article) {
+function renderArticles() {
   const articleSection = document.getElementById('article-section');
-  const card = document.createElement('div');
-  card.classList.add('article-card');
 
-  const cardContent = `
-    <article>
-    <h2>${article.name}</h2>
-    <p>Article ID: ${article.id}</p>
-    </article>
-  `;
-  card.innerHTML = cardContent;
-  articleSection.appendChild(card);
+  articles.forEach((article) => {
+    const card = document.createElement('article');
+    card.classList.add('article-card');
+
+    const articleContent = `
+      <div class="article-image">
+        <img src="${article.image}" class="img-fluid" alt="Article Image">
+      </div>
+      <div class="article-content">
+        <h2>${article.name}</h2>
+        <p>${article.description}</p>
+        <a href="${article.link}" class="read-more">Read More</a>
+      </div>
+    `;
+
+    card.innerHTML = articleContent;
+    articleSection.insertBefore(card, articleSection.firstChild);
+  });
 }
 
-articles.forEach((article) => {
-  articleCards(article);
-});
+renderArticles();
