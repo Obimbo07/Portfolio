20d979992d879aa5b3e5a822ab391b48ad679da6
Obimbo07 <obimboausts@gmail.com>
2024-05-16 14:19:11 +0300

    font sytling and header fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const popup = document.getElementById('navMenu');
-const navMenu = document.getElementById('navMenu');
+// const popup = document.getElementById('navMenu');
+// const navMenu = document.getElementById('navMenu');
 
-const menudissapear = document.getElementById('nav-btn');
+// const menudissapear = document.getElementById('nav-btn');
 const slides = document.querySelectorAll('.imgslider-cards');
 
 let currentSlide = 0;
@@ -35,18 +35,18 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 });
 
-function openMenu() {
-  popup.classList.add('dropdown-menuShow');
-  navMenu.classList.add('navMenu');
-  menudissapear.classList.add('navMenu');
-}
-openMenu();
-function closeMenu() {
-  popup.classList.remove('dropdown-menuShow');
-  navMenu.classList.remove('navMenu');
-  menudissapear.classList.remove('navMenu');
-}
-closeMenu();
+// function openMenu() {
+//   popup.classList.add('dropdown-menuShow');
+//   navMenu.classList.add('navMenu');
+//   menudissapear.classList.add('navMenu');
+// }
+// openMenu();
+// function closeMenu() {
+//   popup.classList.remove('dropdown-menuShow');
+//   navMenu.classList.remove('navMenu');
+//   menudissapear.classList.remove('navMenu');
+// }
+// closeMenu();
 // Project data array
 const projects = [
   {
